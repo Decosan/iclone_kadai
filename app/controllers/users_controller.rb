c5dcefa4764c0a255727e39f1c99656b73decded
Decosan <junx0401@gmail.com>
2019-07-19 12:19:25 +0900

    user_controller 登録、index view設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all.page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] ='Sign up!'
+      redirect_to users_path
+    else
+      flash.now[:danger] ='Failed..'
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] ='Edit completed!'
+      redirect_to users_path(@user)
+    else
+      flash.now[:danger] ='Failed..'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] ="Your Profile has been deleted!"
+    redirect_to new_user_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation,:image,:image_cache)
+  end
+end
