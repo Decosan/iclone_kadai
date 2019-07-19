d22916f6012e1aaa1bad7a9b8309f7559e52ebda
Decosan <junx0401@gmail.com>
2019-07-19 18:06:27 +0900

    post edit update destroy設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :require_user_login
+  before_action :post_identify, only:[:edit,:update,:destroy]
+
   def index
   end
 
@@ -23,14 +26,31 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.update(post_params)
+      flash[:success] ='Edit complete!'
+      redirect_to root_path
+    else
+      flash.now[:danger] ='Failed..'
+      render 'edit'
+    end
   end
 
   def destroy
+    @post.destroy
+    flash[:danger] ='Post deleted!!'
+    redirect_to root_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title,:content,:image,:image_cache)
+  end
+
+  def post_identify
+    @post = current_user.posts.find_by(id: params[:id])
+    unless @post
+      redirect_to root_path
+    end
   end
 end
