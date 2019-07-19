aee360c664e55a95dbd001a4f2665b57eb8bf58d
Decosan <junx0401@gmail.com>
2019-07-19 11:22:04 +0900

    usersテーブルmigration,validation設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+  before_validation {email.downcase!}
+
+  validates :name, presence: true, length: {maximum: 25}
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: true
+
 end
