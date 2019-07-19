42ba116e320d7141c8eafc762835762a0c6641f3
Decosan <junx0401@gmail.com>
2019-07-19 16:13:57 +0900

    Postモデル、コントローラー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   before_action :require_user_login, only:[:index,:show,:edit]
   before_action :user_identify, only:[:show,:edit]
+  before_action :set_user, only:[:show,:edit,:update,:destroy]
 
   def index
     @users = User.all.page(params[:page])
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -26,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] ='Edit completed!'
       redirect_to user_path(@user)
@@ -41,13 +39,16 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     flash[:danger] ="Your Profile has been deleted!"
     redirect_to new_user_path
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation,:image,:image_cache)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_022531) do
+ActiveRecord::Schema.define(version: 2019_07_19_070827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +34,5 @@ ActiveRecord::Schema.define(version: 2019_07_19_022531) do
     t.string "image"
   end
 
+  add_foreign_key "posts", "users"
 end
